8d41d462b4d817c3830ec3beaa789d85a2f3a0c3
Raphael Collet <rco@odoo.com>
2020-10-23 11:20:03 +0200

    do not trigger recomputations upon idempotent assignment

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -3044,6 +3044,30 @@ class TestComputeQueries(common.TransactionCase):
             record = model.create({'foo': 'Foo', 'bar': 'Bar'})
         self.assertEqual(record.bar, 'Bar')
 
+        # do not trigger recomputation when writing the same value
+        with self.assertQueries([]):
+            record.foo = 'Foo'
+        self.assertEqual(record.bar, 'Bar')
+
+        with self.assertQueries([]):
+            record.write({'foo': 'Foo'})
+        self.assertEqual(record.bar, 'Bar')
+
+        # write and trigger recomputation
+        with self.assertQueries([update(model, 'foo', 'bar')]):
+            record.write({'foo': 'One'})
+        self.assertEqual(record.bar, 'One')
+
+        # write and do not trigger recomputation
+        with self.assertQueries([update(model, 'foo', 'bar')]):
+            record.write({'foo': 'Two', 'bar': 'Bar'})
+        self.assertEqual(record.bar, 'Bar')
+
+        # write and do not trigger recomputation (field 'bar' must be protected)
+        with self.assertQueries([update(model, 'foo')]):
+            record.write({'foo': 'One', 'bar': 'Bar'})
+        self.assertEqual(record.bar, 'Bar')
+
     def test_compute_inverse(self):
         model = self.env['test_new_api.compute.inverse']
         model.create({})
@@ -3073,6 +3097,7 @@ class TestComputeQueries(common.TransactionCase):
             record = model.create({'foo': 'Foo', 'bar': 'Bar'})
         self.assertEqual(record.foo, 'Bar')
         self.assertEqual(record.bar, 'Bar')
+
 
 class test_shared_cache(TransactionCaseWithUserDemo):
     def test_shared_cache_computed_field(self):
@@ -3109,3 +3134,156 @@ class test_shared_cache(TransactionCaseWithUserDemo):
                 line.amount = 2
             # The new value for total_amount, should be 3, not 2.
             self.assertEqual(task_form.total_amount, 2)
+
+
+class TestFieldFilterNotEqual(common.TransactionCase):
+    def test_filter_not_equal_char(self):
+        model = self.env['test_new_api.compute.readwrite']
+        field = model._fields['foo']
+        record = model.create({'foo': 'Foo', 'bar': 'Bar'})
+        record.flush()
+
+        self.assertFalse(field.filter_not_equal(record, 'Foo'))
+        self.assertTrue(field.filter_not_equal(record, 'Bar'))
+
+        self.env.clear()
+        self.assertTrue(field.filter_not_equal(record, 'Bar'))
+
+    def test_filter_not_equal_one2many(self):
+        model = self.env['test_new_api.model_active_field']
+        field = model._fields['children_ids']
+        record = model.create({})
+        line1 = model.create({'name': '1', 'parent_id': record.id})
+        line2 = model.create({'name': '2', 'parent_id': record.id})
+        line3 = model.create({'name': '3'})
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertFalse(field.filter_not_equal(line1, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertFalse(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
+
+        # same tests on a new record with value in cache
+        record = record.new(origin=record)
+        line1 = line1.new(origin=line1)
+        line2 = line2.new(origin=line2)
+        line3 = line3.new(origin=line3)
+        (record + line1).children_ids
+        line1.name
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertFalse(field.filter_not_equal(line1, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertFalse(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
+
+        # same tests on a new record without value in cache
+        self.env.cache.invalidate()
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertTrue(field.filter_not_equal(line1, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
+
+    def test_filter_not_equal_many2many(self):
+        line1 = self.env['test_new_api.multi.tag'].create({'name': '1'})
+        line2 = self.env['test_new_api.multi.tag'].create({'name': '2'})
+        line3 = self.env['test_new_api.multi.tag'].create({'name': '3'})
+        model = self.env['test_new_api.multi.line']
+        field = model._fields['tags']
+        record = model.create({'tags': [(4, line1.id), (4, line2.id)]})
+        record0 = model.create({})
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(3, line3.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertFalse(field.filter_not_equal(record0, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertFalse(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
+
+        # same tests on a new record with value in cache
+        record = record.new(origin=record)
+        record0 = record.new(origin=record)
+        line1 = line1.new(origin=line1)
+        line2 = line2.new(origin=line2)
+        line3 = line3.new(origin=line3)
+        (record + record0).tags
+        line1.name
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertFalse(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(3, line3.id)]))
+        self.assertFalse(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertFalse(field.filter_not_equal(record0, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertFalse(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
+
+        # same tests on a new record without value in cache
+        self.env.cache.invalidate()
+
+        self.assertFalse(field.filter_not_equal(record, []))
+        self.assertTrue(field.filter_not_equal(record, [(0, 0, {})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '1'})]))
+        self.assertTrue(field.filter_not_equal(record, [(1, line1.id, {'name': '3'})]))
+        self.assertTrue(field.filter_not_equal(record, [(2, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(3, line3.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line1.id)]))
+        self.assertTrue(field.filter_not_equal(record, [(4, line3.id)]))
+        self.assertTrue(field.filter_not_equal(record0, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(5,)]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line1.id, line2.id, line3.id])]))
+        self.assertTrue(field.filter_not_equal(record, [(6, 0, [line3.id])]))
