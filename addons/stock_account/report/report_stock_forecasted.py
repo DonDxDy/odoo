044b09e0f6b353a88388d825f89d4377720ecc64
svs-odoo <svs@odoo.com>
2020-10-28 13:56:36 +0100

    [FIX] stock_account: currency in forecast report
    Before this commit, the SVL domain wasn't correctly set. So we got back
    SVL for all companies, which can lead to an error if the companies use
    different currencies.
    
    How to reproduce:
      - Create a new company with a different currency than the existing one
      - For a product, define a cost and add some qty. on hand for the two
        companies;
      - Display the Forecast Report for this product -> traceback.
    
    To fix that, we use `product.product` `value_svl` field instead of
    recomputing the value from the SVL and we do that only for the current
    selected warehouse's company, so we deal with only one currency at the
    time.

--- addons/stock_account/report/report_stock_forecasted.py
+++ addons/stock_account/report/report_stock_forecasted.py
@@ -11,10 +11,22 @@ class ReplenishmentReport(models.AbstractModel):
     def _compute_draft_quantity_count(self, product_template_ids, product_variant_ids, wh_location_ids):
         """ Overrides to computes the valuations of the stock. """
         res = super()._compute_draft_quantity_count(product_template_ids, product_variant_ids, wh_location_ids)
-        domain = self._product_domain(product_template_ids, product_variant_ids)
-        svl = self.env['stock.valuation.layer'].search(domain)
-        currency = svl.currency_id or self.env.company.currency_id
-        value = float_repr(sum(svl.mapped('value')), precision_digits=currency.decimal_places)
+        warehouse_id = self.env.context.get('warehouse', False)
+        if warehouse_id:
+            warehouse = self.env['stock.warehouse'].browse(warehouse_id)
+        company = warehouse.company_id if warehouse else self.env.company
+        currency = company.currency_id
+        domain = []
+        if product_template_ids:
+            domain = [('product_tmpl_id', 'in', product_template_ids)]
+        elif product_variant_ids:
+            domain = [('id', 'in', product_variant_ids)]
+        svl_values = self.env['product.product'].with_company(company).search_read(
+            domain,
+            ['value_svl']
+        )
+        value = sum([svl['value_svl'] for svl in svl_values])
+        value = float_repr(value, precision_digits=currency.decimal_places)
         if currency.position == 'after':
             value = '%s %s' % (value, currency.symbol)
         else:
