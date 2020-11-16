e1cfff7e856f2d578a84e2a154826f0d0958f5fd
Thibault Delavallée <tde@odoo.com>
2020-11-16 16:08:40 +0100

    [MOV] website_event_track_exhibitor: arise, website_event_exhibitor !
    RATIONALE
    
    Events often have sponsors displayed on their front page. This should be
    a core feature of Online Event application (website_event).
    
    PURPOSE
    
    Sponsors on event cannot be used without tracks managements. Purpose
    of this task is to allow to use advanced sponsor management (exhibitors)
    without any dependency on tracks management.
    
    SPECIFICATIONS
    
    Rename website_event_track_exhibitor to website_event_exhibitor and remove
    dependency on website_event_track.
    
    LINKS
    
    Task ID-2326433
    COM PR odoo/odoo#61781
    ENT pr odoo/enterprise#14752
    UPG PR odoo/upgrade#1931

--- addons/website_event_exhibitor/__manifest__.py
+++ addons/website_event_exhibitor/__manifest__.py
@@ -11,7 +11,7 @@
     'website': 'https://www.odoo.com/page/events',
     'description': "",
     'depends': [
-        'website_event_track',
+        'website_event',
         'website_jitsi',
     ],
     'data': [
