e764d43101cd37e2dcabd2403648e4a93e70ea22
mir-odoo <mir@odoo.com>
2020-11-19 18:48:18 +0530

    [IMP] Calendar mobile view.

--- addons/web/static/src/js/views/calendar/calendar_quick_create.js
+++ addons/web/static/src/js/views/calendar/calendar_quick_create.js
@@ -29,7 +29,7 @@ var QuickCreate = Dialog.extend({
      * @param {Object} dataTemplate
      * @param {Object} dataCalendar
      */
-    init: function (parent, buttons, options, dataTemplate, dataCalendar) {
+    init: function (parent, buttons, options, dataTemplate, dataCalendar) {debugger
         this._buttons = buttons || false;
         this.options = options;
 
