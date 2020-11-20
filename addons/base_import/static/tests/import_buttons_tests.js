25885c02187dde8147b63b910cbbb0d89a35078f
Mohammed Shekha <msh@odoo.com>
2020-11-20 19:13:54 +0530

    [IMP] Improve tests

--- addons/base_import/static/tests/import_buttons_tests.js
+++ addons/base_import/static/tests/import_buttons_tests.js
@@ -8,6 +8,7 @@ const PivotView = require('web.PivotView');
 const testUtils = require('web.test_utils');
 
 const cpHelpers = testUtils.controlPanel;
+const createActionManager = testUtils.createActionManager;
 const createView = testUtils.createView;
 
 QUnit.module('Base Import Tests', {
@@ -22,29 +23,94 @@ QUnit.module('Base Import Tests', {
                 ]
             },
         };
+        this.actions = [{
+            id: 1,
+            name: 'Partners Action 1',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[1, 'list']],
+        }, {
+            id: 2,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[2, 'list']],
+        }, {
+            id: 3,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[3, 'list']],
+        }, {
+            id: 4,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[4, 'kanban']],
+        }, {
+            id: 5,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[5, 'kanban']],
+        }, {
+            id: 6,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[6, 'kanban']],
+        }, {
+            id: 7,
+            name: 'Partners',
+            res_model: 'foo',
+            type: 'ir.actions.act_window',
+            views: [[false, 'pivot']],
+        }];
+
+        this.archs = {
+            // list views
+            'foo,1,list': '<tree><field name="foo"/></tree>',
+            'foo,2,list': '<tree create="0"><field name="foo"/></tree>',
+            'foo,3,list': '<tree import="0"><field name="foo"/></tree>',
+
+            // kanban views
+            'foo,4,kanban': '<kanban><templates><t t-name="kanban-box">' +
+                '<div class="oe_kanban_global_click"><field name="foo"/></div>' +
+                '</t></templates></kanban>',
+            'foo,5,kanban': '<kanban><templates><t t-name="kanban-box">' +
+                '<div class="oe_kanban_global_click"><field name="foo"/></div>' +
+                '</t></templates></kanban>',
+
+            // pivot views
+            'foo,false,pivot': '<pivot><field name="foobar" type="measure"/></pivot>',
+
+            // search views
+            'foo,false,search': '<search></search>',
+        };
     }
 });
 
 QUnit.test('import in favorite dropdown in list', async function (assert) {
     assert.expect(2);
 
-    const list = await createView({
-        View: ListView,
-        model: 'foo',
+    var actionManager = await createActionManager({
+        actions: this.actions,
+        archs: this.archs,
         data: this.data,
-        arch: '<tree><field name="foo"/></tree>',
     });
 
-    testUtils.mock.intercept(list, 'do_action', function () {
+    testUtils.mock.intercept(actionManager, 'do_action', function () {
         assert.ok(true, "should have triggered a do_action");
     });
 
-    await testUtils.dom.click(list.$('.o_favorite_menu button'));
-    assert.containsOnce(list, '.o_import_menu');
+    await actionManager.doAction(1);
 
-    await testUtils.dom.click(list.$('.o_import_menu button'));
+    await cpHelpers.toggleFavoriteMenu(actionManager);
+    assert.containsOnce(actionManager, '.o_import_menu');
 
-    list.destroy();
+    await testUtils.dom.click(actionManager.$('.o_import_menu button'));
+
+    actionManager.destroy();
 });
 
 QUnit.test('import favorite dropdown item should not in list with create="0"', async function (assert) {
