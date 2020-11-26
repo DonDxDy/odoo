9dae5d29e30c9e873bb078f5312286b52d2bca2e
Thibault Libioulle <tle@odoo.com>
2020-11-26 18:03:30 +0100

    [IMP] project : Improve sub tasks creation and workflow
    1) With this commit we want to change the way to link a subtask with its project
    	for UI reasons, we want ot be able to easily distinguish subtasks that are linked to the project of their parent from subtasks that are linked to another project
    	To do so, the idea is to make the field project_id not required anymore. Only relevant tasks are linked to their project, the other are linked to their parent which are linked to a project.
    	As a result, domains in the application are only based on the fact that tasks have project_id. The other are simply hidden.
    
    	This is a threat to both security and reporting on several workflows.
    	Regarding security : Some Record Rules are based on the project configuration. If project is not linked to a task, the rule is broken and not relevant anymore.
    	Regarding reporting : If we want to have all tasks of a project, both parent and sub tasks, we have to use some child_of queries which threaten the perfomances.
    	Also, with a smaller impact, we have to change some related fields in the task model into computed fields, again a loss of performance.
    
    	To cover those losses, a new field has been created : the effective_project_id which will be the field to use for security reasons or consolidation of tasks with projects.
    
    2) The stage of a task is the same as the one of its parent if project is not set.
    
    3) The number of subtasks is shown on kanban and list views with a wigdet to easily render it and extend the UI.
    
    4) The default sub-task assignee is the assignee of the parent task.
    
    5) With this commit we want to remove the subtask global setting. Sub-tasks are enabled by default but can be disabled by project.
    
    6) Task form is modified to integrate those changes.
    
    task-2388034 : sub-task of : task-2379043

--- addons/project/tests/test_multicompany.py
+++ addons/project/tests/test_multicompany.py
@@ -251,19 +251,19 @@ class TestMultiCompanyProject(TestMultiCompanyCommon):
     def test_create_subtask(self):
         with self.sudo('employee-a'):
             with self.allow_companies([self.company_a.id, self.company_b.id]):
-                # worklow tested in this test is no more possible with new subtask
-                with self.assertRaises(UserError, msg="A sub-task cannot be linked to another project than its parent task."):
-                    # create subtask, set parent; the onchange will set the correct company and subtask project
-                    with Form(self.env['project.task'].with_context({'tracking_disable': True, 'default_parent_id': self.task_1.id, 'default_project_id': self.project_company_b.id})) as task_form:
-                        task_form.name = 'Test Subtask in company B'
-                    task = task_form.save()
+                # create subtask, set parent; the onchange will set the correct company and subtask project
+                with Form(self.env['project.task'].with_context({'tracking_disable': True, 'default_parent_id': self.task_1.id, 'default_project_id': self.project_company_b.id})) as task_form:
+                    task_form.name = 'Test Subtask in company B'
+                task = task_form.save()
+
+                self.assertEqual(task.company_id, self.project_company_b.company_id, "The company of the subtask should be the one from its project, and not from its parent.")
 
                 # set parent on existing orphan task; the onchange will set the correct company and subtask project
-                with Form(self.env['project.task'].with_context({'tracking_disable': True, 'default_parent_id': self.task_1.id})) as task_form:
+                with Form(self.env['project.task'].with_context({'default_parent_id': self.task_1.id})) as task_form:
                     task_form.name = 'Test Task 2 becomes child of Task 1 (other company)'
                 task = task_form.save()
 
-                self.assertEqual(task.company_id, task.project_id.company_id, "The company of the orphan subtask should be the one from its project.")
+                self.assertEqual(task.company_id, task.effective_project_id.company_id, "The company of the orphan subtask should be the one from its project.")
 
     def test_cross_subtask_project(self):
         # set up default subtask project
@@ -276,8 +276,8 @@ class TestMultiCompanyProject(TestMultiCompanyCommon):
 
                 task = task_form.save()
 
-                self.assertEqual(task.project_id, self.task_1.project_id, "The default project of a subtask should be the default subtask project of the project from the mother task")
-                self.assertEqual(task.company_id, task.project_id.company_id, "The company of the orphan subtask should be the one from its project.")
+                self.assertEqual(task.effective_project_id, self.task_1.project_id, "The default project of a subtask should be the default subtask project of the project from the mother task")
+                self.assertEqual(task.company_id, task.effective_project_id.company_id, "The company of the orphan subtask should be the one from its project.")
                 self.assertEqual(self.task_1.child_ids.ids, [task.id])
 
         with self.sudo('employee-a'):
