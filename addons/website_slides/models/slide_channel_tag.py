189876c28f3639aa310c6eb6cfe7fab8c74e812b
ijas ahammed <ija@odoo.com>
2020-12-01 14:53:59 +0530

    [ADD] website_slides: separate backend tags from frontend tags
    now in kanban view we can hide the tags while "hide for kanban"
    is enabled, while creating new tags defaultly they had no colors
    but now they will have colors defaultly that means "hide for kanban"
    is defaultly false now.
    In frontend while a user added a tag it was not shown in the frontend
    because defaultly the tag was created without any color, now as tags are
    created with color they will be displayed in the frontend as the tags
    have color, so by this the confussion is resolved, no one can create
    colorless tags from frontend.
    
    Task-id: 2357942

--- addons/website_slides/models/slide_channel_tag.py
+++ addons/website_slides/models/slide_channel_tag.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from random import randint
 
 
 class SlideChannelTagGroup(models.Model):
@@ -23,6 +24,9 @@ class SlideChannelTag(models.Model):
     _description = 'Channel/Course Tag'
     _order = 'group_sequence asc, sequence asc'
 
+    def _get_default_color(self):
+        return randint(1, 11)
+
     name = fields.Char('Name', required=True, translate=True)
     sequence = fields.Integer('Sequence', default=10, index=True, required=True)
     group_id = fields.Many2one('slide.channel.tag.group', string='Group', index=True, required=True)
@@ -30,4 +34,5 @@ class SlideChannelTag(models.Model):
         'Group sequence', related='group_id.sequence',
         index=True, readonly=True, store=True)
     channel_ids = fields.Many2many('slide.channel', 'slide_channel_tag_rel', 'tag_id', 'channel_id', string='Channels')
-    color = fields.Integer(string='Color Index', help="Color to apply to this tag (including in website).")
+    color = fields.Integer(string='Color Index', default=_get_default_color,
+                           help="Color to apply to this tag (including in website).")
